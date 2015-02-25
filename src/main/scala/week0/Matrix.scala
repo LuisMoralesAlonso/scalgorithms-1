466f0d6cd2c0623f57f300005054996998c71d53
LuisMoralesAlonso <l.morales@bbva.com>
2015-02-25 16:57:21 +0100

    Matrix Final Version. Without Jama

--- src/main/scala/week0/Matrix.scala
+++ src/main/scala/week0/Matrix.scala
@@ -13,12 +13,6 @@ object Matrix{
   /** effectively add RichMatrix methods to List[List[Double]] */
   implicit def pimp1(m:Matrix) = new RichMatrix(m)
 
-  implicit def pimp2(m:Matrix) = new JamaMatrix( m, null )
-
-  //implicit def pimp3(jm:JamaMatrix) = new RichMatrix(jm.asMatrix)
-
-  //implicit def pimp4(jm:JamaMatrix) = jm.asMatrix
-
   def apply( rowCount:Int, colCount:Int )( f:(Int,Int) => Double ) = (
       for(i <- 1 to rowCount) yield 
   ( for( j <- 1 to colCount) yield f(i,j) ).toList
