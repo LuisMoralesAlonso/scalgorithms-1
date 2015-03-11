a3555ee222d59f298ce1ba6b14b2ec1d53e2940e
LuisMoralesAlonso <l.morales@bbva.com>
2015-03-11 11:18:18 +0100

    Quick-Union as a class

--- src/main/scala/week1/SequenceIntervals.scala
+++ src/main/scala/week1/SequenceIntervals.scala
@@ -3,22 +3,25 @@ package week1
 /**
  * @author luis
  */
-  abstract class SeqIntervals[T](seq: Seq[T]) {
-    def find(index: Int): T    
-    def connected(p: Int, q: Int): Boolean = find(p)==find(q)
-    def union(p: Int, q: Int): SeqIntervals[T]    
-    def count: Int = seq.distinct.size 
-    override def toString = seq.toString()
-  }
+abstract class SeqIntervals(seq: Seq[Int]) {
+  def find(index: Int): Int
+  def connected(p: Int, q: Int): Boolean = find(p) == find(q)
+  def union(p: Int, q: Int): SeqIntervals
+  def count: Int = seq.distinct.size
+  override def toString = seq.toString()
+}
 
-  class QuickFind[T](seq: Seq[T]) extends SeqIntervals[T](seq: Seq[T]) {
-    def find(p: Int): T = seq(p)
-    def union(p: Int, q: Int): SeqIntervals[T] = 
-      if (connected(p, q)) this else new QuickFind(seq map (r => if (r == find(p)) find(q) else r))
+class QuickFind(seq: Seq[Int]) extends SeqIntervals(seq: Seq[Int]) {
+  def find(p: Int): Int = seq(p)
+  def union(p: Int, q: Int): SeqIntervals =
+    if (connected(p, q)) this else new QuickFind(seq map (r => if (r == find(p)) find(q) else r))
+}
+
+class QuickUnion(seq: Seq[Int]) extends SeqIntervals(seq: Seq[Int]) {
+  def find(p: Int): Int = p match {
+    case index if index == seq(index) => index
+    case index => find(seq(index))
   }
-  
-  class QuickUnion[T](seq: Seq[T]) extends SeqIntervals[T](seq: Seq[T]) {
-    def find(p: Int): T = seq(p)
-    def union(p: Int, q: Int): SeqIntervals[T] = 
-      if (connected(p, q)) this else new QuickFind(seq map (r => if (r == find(p)) find(q) else r))
-  }
+  def union(p: Int, q: Int): SeqIntervals =
+    if (connected(p, q)) this else new QuickUnion(seq updated (p,seq(q)))
+}
