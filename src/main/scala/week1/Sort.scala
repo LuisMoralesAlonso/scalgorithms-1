caa7a644419c716d4a08c58b43eabd4983d5f3be
LuisMoralesAlonso <l.morales@bbva.com>
2015-03-24 16:39:34 +0100

    With pure functional Insertion Sort

--- src/main/scala/week1/Sort.scala
+++ src/main/scala/week1/Sort.scala
@@ -5,23 +5,31 @@ package week1
  */
 
 import scala.reflect._
+
 object Sort {
-  def selectionSort[A <% Ordered[A]](lista: Seq[A]): Seq[A] = {
-    def stepOrder(acc: Seq[A], lista: Seq[A]): Seq[A] = lista match {
-      case Nil => acc
-      case _   => lista.min +: stepOrder(acc, lista.diff(Seq(lista.min)))
+  def selectionSort[A <% Ordered[A]](lista: Array[A])(implicit ctag: ClassTag[A]): Array[A] = {
+    def stepOrder(acc: Array[A], lista: Array[A]): Array[A] = lista match {
+      case lista if lista.isEmpty => acc
+      case _                      => lista.min +: stepOrder(acc, lista.diff(Array(lista.min)))
     }
-    stepOrder(Seq(), lista)
+    stepOrder(Array(), lista)
   }
 
-  def insertionSort[A <%Ordered[A]](xs: Array[A])(implicit ctag: ClassTag[A]): Array[A] = {
+  def quickSort[A <% Ordered[A]](xs: Array[A])(implicit ctag: ClassTag[A]): Array[A] = {
     if (xs.length <= 1) xs
     else {
       val pivot = xs(xs.length / 2)
       Array.concat(
-        insertionSort(xs filter (pivot >)),
+        quickSort(xs filter (pivot >)),
         xs filter (pivot ==),
-        insertionSort(xs filter (pivot <)))
+        quickSort(xs filter (pivot <)))
     }
   }
+
+  def insertionSort[A <% Ordered[A]](xs: List[A]): List[A] = {
+    def insert(list: List[A], value: A) = list.span(x => x < value) match {
+      case (lower, upper) => lower ::: value :: upper
+    }
+    xs.foldLeft(List.empty[A])(insert)
+  }                             
 }
